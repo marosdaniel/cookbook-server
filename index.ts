34f03fe40cf140a2197eb1ab24f8d0e6f1167088
Maros Dániel <marosdaniel@gmail.com>
2023-09-14 12:36:25 +0200

    adding expressMiddleware

--- index.ts
+++ index.ts
@@ -1,16 +1,19 @@
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
-import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
-import { loadSchema } from '@graphql-tools/load';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
+import { loadSchema } from '@graphql-tools/load';
 
 import context from './server/context';
 
-import resolvers from './server/graphql/resolvers';
-
 dotenv.config();
-
+import resolvers from './server/graphql/resolvers';
 const MONGO_DB = process.env.MONGO_URI;
 const PORT: number = +process.env.PORT || 8080;
 
@@ -24,22 +27,38 @@ const schema = await loadSchema('server/graphql/**/*.graphql', {
 
 // TODO: add CORS config
 // https://www.apollographql.com/docs/apollo-server/security/cors/
+const app = express();
+const httpServer = http.createServer(app);
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
+await server.start();
+
+// Set up our Express middleware to handle CORS, body parsing,
+// and our expressMiddleware function.
+app.use(
+  '/',
+  cors<cors.CorsRequest>(),
+  // 50mb is the limit that `startStandaloneServer` uses, but you may configure this to suit your needs
+  bodyParser.json({ limit: '50mb' }),
+  // expressMiddleware accepts the same arguments:
+  // an Apollo Server instance and optional configuration options
+  expressMiddleware(server, {
+    // if the playground is open, it runs introspection against the graph at an interval
+    // not a problem
+    context,
+  }),
+);
+
 mongoose
-  .connect(MONGO_DB)
+  .connect(MONGO_DB.toString())
   .then(() => {
     console.log('Connected to MongoDB...');
-    return startStandaloneServer(server, {
-      // if the playground is open, it runs introspection against the graph at an interval
-      // not a problem
-      context,
-      listen: { port: PORT },
-    });
   })
-  .then(server => {
-    console.log(`🚀  Server ready at: ${server.url}`);
+  .then(() => {
+    new Promise<void>(resolve => httpServer.listen({ port: PORT }, resolve));
+    console.log(`🚀 Server ready at http://localhost:8080/`);
   });
