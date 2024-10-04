62b09a2ca7be66f69de47a9058d1e25fd3e04700
Maros Dániel <marosdaniel@gmail.com>
2024-10-04 20:24:39 +0200

    refactor

--- server/graphql/resolvers/recipes.ts
+++ server/graphql/resolvers/recipes.ts
@@ -8,6 +8,7 @@ import {
   getRecipesByUserId,
   getRecipesByUserName,
 } from './recipe/queries';
+import { createRecipe } from './recipe/mutations1';
 
 const recipeResolvers = {
   Query: {
@@ -28,78 +29,7 @@ const recipeResolvers = {
     // },
   },
   Mutation: {
-    async createRecipe(
-      _,
-      {
-        recipeCreateInput: {
-          title,
-          description,
-          ingredients,
-          preparationSteps,
-          category,
-          imgSrc,
-          labels,
-          cookingTime,
-          difficultyLevel,
-          servings,
-          youtubeLink,
-        },
-      },
-      context,
-    ) {
-      if (
-        !title ||
-        !description ||
-        !ingredients ||
-        !preparationSteps ||
-        !category ||
-        !cookingTime ||
-        !difficultyLevel ||
-        !servings
-      ) {
-        throwCustomError('All fields are required', ErrorTypes.BAD_REQUEST);
-      }
-      try {
-        const user = await User.findById(context._id);
-        if (!user) {
-          throwCustomError('Unauthenticated operation - no user found', ErrorTypes.UNAUTHENTICATED);
-        }
-
-        const labelsFromDb = await Metadata.find({ key: { $in: labels.map(label => label.value) } });
-        const categoryFromDb = await Metadata.findOne({ key: category.value });
-        const difficultyLevelFromDb = await Metadata.findOne({ key: difficultyLevel.value });
-
-        const newDate = new Date().toISOString();
-
-        const newRecipe = new Recipe({
-          title,
-          description,
-          ingredients,
-          preparationSteps,
-          createdBy: user.userName,
-          createdAt: newDate,
-          updatedAt: newDate,
-          category: categoryFromDb,
-          imgSrc,
-          labels: labelsFromDb,
-          cookingTime,
-          difficultyLevel: difficultyLevelFromDb,
-          servings,
-          youtubeLink,
-          averageRating: 0.0,
-          ratingsCount: 0,
-        });
-
-        const res = await newRecipe.save();
-
-        user.recipes.push(res);
-        await user.save();
-
-        return res;
-      } catch (error) {
-        throw new Error(error);
-      }
-    },
+    createRecipe,
 
     async editRecipe(
       _,
